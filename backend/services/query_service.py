be084b873658be32ec317073776326ed7b35ef8c
key4127 <2827092384@qq.com>
2025-08-27 21:36:44 +0800

    initialize backend

--- backend/services/query_service.py
+++ backend/services/query_service.py
@@ -2,9 +2,9 @@ from fastapi import Depends
 from llama_index.core.query_engine import BaseQueryEngine
 
 async def query(query: str, query_engine: BaseQueryEngine):
-   print(query)
-   response = query_engine.query(query)
-   return {
+    print(query)
+    response = query_engine.query(query)
+    return {
        "query": query,
        "answer": str(response)
-   }
+    }
