36ae5a720ec2349185f20569e83a3f405c648cc9
key4127 <2827092384@qq.com>
2025-08-27 16:31:56 +0800

    pass the query engine test

--- backend/test/query_engine_test.py
+++ backend/test/query_engine_test.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
 from llama_index.core import Settings as LlamaIndexSettings
-from llama_index.llms.openai_like import OpenAILike
 from backend.dependencies import get_query_engine
 from backend.config import Settings
 from backend.core.embedding.embedding_model import EmbeddingModel
-from backend.core.storage.weaviate import WeaviateStorage
+from llama_index.llms.deepseek import DeepSeek
 
 query = "I want to learn about CS, please give me some resources. Please answer in Chinese."
 
@@ -15,11 +14,7 @@ def query_engine_test(query: str) -> None:
     settings = Settings()
     embedding_model = EmbeddingModel(settings.embedding_model)
 
-    LlamaIndexSettings.llm = OpenAILike(
-        model="deepseek-chat",
-        api_base="https://api.deepseek.com/beta",
-        api_key=os.getenv("DEEPSEEK_API_KEY"),
-    )
+    LlamaIndexSettings.llm = DeepSeek(model="deepseek-chat", api_key=os.getenv("DEEPSEEK_API_KEY"))
     LlamaIndexSettings.embed_model = embedding_model.get_embedding_model()
 
     query_engine = get_query_engine()
